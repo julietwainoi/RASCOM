e14216e95b616d7968b0f2d674a657d32745c58e
julietwainoi <juliewainoi@gmail.com>
2023-10-09 13:00:19 +0300

    customized the navbar

--- bustani.js
+++ bustani.js
@@ -12,3 +12,19 @@ if (bar) {
     })
 }
 
+var index = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) { // Use < instead of >
+        x[i].style.display = "none";
+    }
+    index++;
+    if (index > x.length) {
+        index = 1;
+    }
+    x[index - 1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
